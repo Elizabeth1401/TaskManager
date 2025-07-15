b1b94b39fe4be55d8b891cf94d333fc9d9b933ae
Elizabeth1401 <140805762+Elizabeth1401@users.noreply.github.com>
2025-07-15 13:34:47 +0200

    Initial setup

---
+++ src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -0,0 +1,25 @@
+package com.example.taskmanager.controller;
+
+import com.example.taskmanager.model.Task;
+import com.example.taskmanager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public Task createTask (@RequestBody Task task){
+        return taskService.addTask(task);
+    }
+}
---
+++ src/main/java/com/example/taskmanager/service/TaskService.java
@@ -0,0 +1,23 @@
+package com.example.taskmanager.service;
+
+import com.example.taskmanager.model.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final List<Task> tasks = new ArrayList<>();
+    private long idCounter = 1;
+
+    public List<Task> getAllTasks(){
+        return tasks;
+    }
+
+    public Task addTask(Task task){
+        task.setId(idCounter++);
+        tasks.add(task);
+        return task;
+    }
+}
